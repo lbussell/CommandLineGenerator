0dc50da72b2df09f65d31377a358738d3337cad3
Logan Bussell <github@loganbussell.com>
2026-02-22 11:57:40 -0800

    Add sample for multiple commands

---
+++ src/Samples/02_SimpleWithMultipleCommands/Program.cs
@@ -0,0 +1,34 @@
+// SPDX-FileCopyrightText: Copyright (c) 2026 Logan Bussell
+// SPDX-License-Identifier: MIT
+
+using System.CommandLine;
+using CommandLineGenerator;
+
+RootCommand rootCommand = new RootCommand();
+rootCommand.AddOptions<RootCommandOptions>();
+rootCommand.SetAction(parseResult =>
+{
+    // Parse the command line arguments into an instance of MyOptionsClass.
+    RootCommandOptions options = CommandLineContext.Parse<RootCommandOptions>(parseResult);
+    Console.WriteLine($"You passed in these options: {options}");
+});
+
+Command listCommand = new Command("list");
+listCommand.AddOptions<ListCommandOptions>();
+listCommand.SetAction(parseResult =>
+{
+    ListCommandOptions options = CommandLineContext.Parse<ListCommandOptions>(parseResult);
+    Console.WriteLine($"You passed in these options: {options}");
+});
+
+rootCommand.Add(listCommand);
+rootCommand.Parse(args).Invoke();
+
+internal sealed record RootCommandOptions(bool Verbose = false);
+
+internal sealed record ListCommandOptions(string Path);
+
+[CommandLineBindable(typeof(RootCommandOptions))]
+[CommandLineBindable(typeof(ListCommandOptions))]
+[CommandLineNamingConvention(CommandLineNamingConvention.KebabCase)]
+internal sealed partial class CommandLineContext : CommandLineBindingContext { }
